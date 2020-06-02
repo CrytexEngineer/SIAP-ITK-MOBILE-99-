bf0d93e9b56bfd29f378b758d48e95682045f7db
Naufan udin <tnaufan@gmail.com>
2020-06-02 13:55:28 +0800

    Most feature working

--- app/src/main/java/com/example/siapitk/ApiUtils/RetrofitInstance.kt
+++ app/src/main/java/com/example/siapitk/ApiUtils/RetrofitInstance.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class RetrofitInstance {
     companion object {
         private var retrofit: Retrofit? = null
-        private const val BASE_URL = "https://94e2a824.ngrok.io/api/v1/mobile/"
+        private const val BASE_URL = "http://192.168.137.1:8000/api/v1/mobile/"
 
         fun getRetrofitInstance(): Retrofit {
             retrofit = retrofit ?: retrofit2.Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
